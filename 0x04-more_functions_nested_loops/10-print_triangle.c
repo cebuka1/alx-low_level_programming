126c16a8f51f375a0524b09ebaf10a92572bb718
cebuka1 <ebusco29@gmail.com>
2023-10-13 20:03:22 -0700

    root@620941ce0062:~/alx-low_level_programming/0x04-more_functions_nested_loops# betty 10-print_triangle.c

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -13,10 +13,6 @@ void print_triangle(int size)
 
 	if (size <= 0)
 	{
-		_putchar('\n');
-	}
-	else
-	{
 		for (row = 1; row <= size; row++)
 		{
 		for (spaces = size - row; spaces >= 1; spaces ==)
