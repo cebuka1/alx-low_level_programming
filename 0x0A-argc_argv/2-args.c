5470c0bbeaacd13319a03207927379ae34419c34
cebuka1 <ebusco29@gmail.com>
2023-10-30 06:35:34 -0700

    2-args.c

--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 /**
  * main - print all arguments it receive
- * argc: number of command line argument
- * argv: array that contains the program command line arg.
+ * argc: The number of command-line argument
+ * argv: An array of strings containing the command-line arguments.
  * Return: (0) when successful.
  */
 int main(int argc, char *argv[])
