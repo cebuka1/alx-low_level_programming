a457dc2278c76bbdf2bfee70c5b4097d7990c464
cebuka1 <ebusco29@gmail.com>
2023-11-21 09:29:37 -0800

    2-str_concat.c

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -1,54 +1,48 @@
 #include "main.h"
 #include "stdlib.h"
 /**
- * str_concat - concertenate two strings
- *
- * @s1: first string
- * @s2: second string
- *
- * Return: pointer to the conc... string
- * which memory was allocated for.
+ * str_concat - get ends of input and add together for size
+ * @s1: input one to concat
+ * @s2: input two to concat
+ * Return: concat of s1 and s2
  */
 char *str_concat(char *s1, char *s2)
 {
-	size_t i, j;
-	char *ptr;
+	char *conct;
+	int i, ci;
 
 	if (s1 == NULL)
-	{
 		s1 = "";
-	}
 	if (s2 == NULL)
-	{
 		s2 = "";
-	}
 
-	len1 = 0;
-	while (s1[len1] != '\0')
-	{
-		len1++;
-	}
+	i = ci = 0;
+	while (s1[i] != '\0')
+		i++;
+	while (s2[ci] != '\0')
+		ci++;
+	conct = malloc(sizeof(char) * (i + ci + 1));
 
-	len2 = 0;
-	while (s2[len2] != '\0')
-	{
-		len2++;
-	}
-
-	ptr = malloc(sizeof(char) * (len1 + len2 + 1));
-	if (ptr == NULL)
-	{
-		free(ptr);
+	if (conct == NULL)
 		return (NULL);
-	}
-	for (i = 0; i < len1; i++)
+	i = ci = 0;
+	while (s1[i] != '\0')
 	{
-		ptr[i] = s1[i];
-	}
-	for (j = 0; j <= len2; j++)
-	{
-		ptr[i] = s2[j];
+		conct[i] = s1[i];
 		i++;
 	}
-	return (ptr);
+
+	while (s2[ci] != '\0')
+	{
+		conct[i] = s1[i];
+		i++;
+	}
+
+	while (s2[ci] != '\0')
+	{
+		conct[i] = s2[ci];
+		i++; ci++;
+	}
+	conct[i] = '\0';
+	return (conct);
 }
---
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -0,0 +1,4 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+ * alloc_grid - return a pointer to a 2 dimenation array
