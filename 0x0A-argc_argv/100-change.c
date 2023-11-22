4117482184aefd38b1fabf839f0ce1e072910495
cebuka1 <ebusco29@gmail.com>
2023-11-22 14:12:09 -0800

    #include <unistd.h>

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -13,7 +13,7 @@
 int main(int argc, char *argv[])
 {
 	int num, j, result;
-	int coins[] = {25, 10, 5, 2, 1}'
+	int coins[] = {25, 10, 5, 2, 1};
 
 	if (argc != 2)
 	{
@@ -39,6 +39,6 @@ int main(int argc, char *argv[])
 		}
 	}
 
-	printf("%d\n",result);
+	printf("%d\n", result);
 	return (0);
 }
