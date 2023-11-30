fa405071383d2ea443d5af15ac4785864048d22a
cebuka1 <ebusco29@gmail.com>
2023-11-30 08:25:13 -0800

    3-main.c, 3-op_functions.c, 3-get_op_func.c, 3-calc.h

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -29,14 +29,14 @@ int main(int argc, char *argv[])
 	if (get_op_func(op) == NULL || op[1] != '\0')
 	{
 		printf("Error\n");
-		exit (99);
+		exit(99);
 	}
 
 	/* if user tries to divide or perform modulus by 0 */
 	if ((*op == '/' && num2 == 0) || (*op == '%' && num2 == 0))
 	{
 		printf("Error\n");
-		exit (100);
+		exit(100);
 	}
 
 	/* print results of operation */
