5963b4e695b6c91ea0ed019dd0b6a1fc41e3cac6
cebuka1 <ebusco29@gmail.com>
2024-01-12 10:32:20 -0800

    2-args.c

--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -3,8 +3,8 @@
 
 /**
  * main - prints all arguments it receives
- * argc: number of arguments
- * argv: array of arguments
+ * argv: number of arguments
+ * argc: array of arguments
  *
  * Return: Always 0 (Success)
  */
